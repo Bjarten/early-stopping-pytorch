b345d0b781db52da40b6f65b141deb2ad4772c17
Bjarten <bms_mok@hotmail.com>
2018-12-30 12:22:54 +0100

    feat: import torch and numpy

--- pytorchtools.py
+++ pytorchtools.py
@@ -1,3 +1,6 @@
+import numpy as np
+import torch
+
 class EarlyStopping:
     """Early stops the training if validation loss dosen't improve after a given patience."""
     def __init__(self,patience=3):
