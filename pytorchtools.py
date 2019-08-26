780b1dc820d3dd9ed80281453c2a0639abfb600f
wolframalpha <wolframalphaV1.0@gmail.com>
2019-08-26 21:21:11 +0530

    Update pytorchtools.py

--- pytorchtools.py
+++ pytorchtools.py
@@ -28,7 +28,7 @@ class EarlyStopping:
         if self.best_score is None:
             self.best_score = score
             self.save_checkpoint(val_loss, model)
-        elif score < self.best_score - delta:
+        elif score < self.best_score - self.delta:
             self.counter += 1
             print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
