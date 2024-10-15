4be4f6e8606cc905d1a4a6a8d99f215cfe4e19e7
Bjarten <BjarteSunde@outlook.com>
2024-10-15 11:26:30 +0900

    refactor: add double newline after imports

--- pytorchtools.py
+++ pytorchtools.py
@@ -4,6 +4,7 @@ __version__ = "0.1.0"
 import numpy as np
 import torch
 
+
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
     def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
