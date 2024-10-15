eb947c61281a57d530d049f1b656dd81354ab096
Bjarten <BjarteSunde@outlook.com>
2024-10-15 11:16:37 +0900

    fix: handle best_val_loss correct

--- pytorchtools.py
+++ pytorchtools.py
@@ -35,18 +35,20 @@ class EarlyStopping:
         if self.best_val_loss is None:
             self.best_val_loss = val_loss
             self.save_checkpoint(val_loss, model)
-        elif val_loss > self.best_val_loss - self.delta:
+        elif val_loss < self.best_val_loss - self.delta:
+            # Significant improvement detected
+            self.best_val_loss = val_loss
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0  # Reset counter since improvement occurred
+        else:
+            # No significant improvement
             self.counter += 1
             self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
-        else:
-            self.best_val_loss = val_loss
-            self.save_checkpoint(val_loss, model)
-            self.counter = 0
 
     def save_checkpoint(self, val_loss, model):
-        '''Saves model when validation loss decrease.'''
+        '''Saves model when validation loss decreases.'''
         if self.verbose:
             self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
         torch.save(model.state_dict(), self.path)
